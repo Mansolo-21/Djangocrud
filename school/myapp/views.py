b5aec8529b98e8cf5c9410cba688b2ce54de08b3
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-25 16:23:40 +0300

    second commit

--- school/myapp/views.py
+++ school/myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from . forms import StudentForm
 # Create your views here.
 def index (request):
     return render(request,'myapp/index.html')
+
+#crud
+#c-create-add a record into db
+def create_student(request):
+    form=StudentForm()
+    return render(request,'myapp/studentform.html',{'form':form})
